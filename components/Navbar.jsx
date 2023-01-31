8ef6f9d3bbee90458ae3758937cc6155d2fc8a4a
ashishxcode <ashish.patel0903@gmail.com>
2023-01-31 18:20:15 +0530

    CI : Adding Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,40 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+	<motion.nav
+		variants={navVariants}
+		initial="hidden"
+		whileInView="show"
+		className={`${styles.xPaddings} py-8 relative`}
+	>
+		<div className="absolute w-[50%] inset-0 gradient-01" />
+		<div
+			className={`${styles.innerWidth} mx-auto flex justify-between 
+      items-center
+    gap-8`}
+		>
+			<img
+				src="/search.svg"
+				alt="search"
+				className="w-[24px] h-[24px] object-contain cursor-pointer"
+			/>
+			<h1
+				className="font-black text-[1.6rem]  leading-[2.5rem]
+      text-white"
+			>
+				METAVERSE
+			</h1>
+			<img
+				src="/menu.svg"
+				alt="menu"
+				className="w-[24px] h-[24px] object-contain cursor-pointer"
+			/>
+		</div>
+	</motion.nav>
 );
 
 export default Navbar;
