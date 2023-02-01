3c442589c5196a54468b193bb9b69fb546dab4a6
ashishxcode <ashish.patel0903@gmail.com>
2023-02-01 10:04:21 +0530

    CI : Adding Hero Section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,47 @@
 'use client';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
 
 const Hero = () => (
-  <section>
-    Hero section
-  </section>
+	<section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+		<motion.div
+			variants={staggerContainer}
+			initial="hidden"
+			whileInView="show"
+			viewport={{
+				once: false,
+				amount: 0.25,
+			}}
+			className={`${styles.innerWidth} mx-auto flex flex-col`}
+		>
+			<div className="flex justify-center items-center flex-col relative z-10">
+				<motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+					Metaverse
+				</motion.h1>
+				<motion.div
+					variants={textVariant(1.2)}
+					className="flex flex-row items-center justify-center"
+				>
+					<h1 className={styles.heroHeading}>MA</h1>
+					<div className={styles.heroDText} />
+					<h1 className={styles.heroHeading}>ness</h1>
+				</motion.div>
+			</div>
+			<motion.div
+				variants={slideIn('right', 'tween', 0.2, 1)}
+				className="relative w-full md:-mt-[20px] -mt-[12px]"
+			>
+				<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px] ">
+					<img
+						src="/cover.png"
+						alt="cover"
+						className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+					/>
+				</div>
+			</motion.div>
+		</motion.div>
+	</section>
 );
 
 export default Hero;
