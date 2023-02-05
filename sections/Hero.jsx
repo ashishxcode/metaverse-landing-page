e87e327cb728aca7afdfba5b43b9a65c06cf756d
ashishxcode <ashish.patel0903@gmail.com>
2023-02-05 17:37:03 +0530

    CI : Adding stamp image in hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -32,13 +32,21 @@ const Hero = () => (
 				variants={slideIn('right', 'tween', 0.2, 1)}
 				className="relative w-full md:-mt-[20px] -mt-[12px]"
 			>
-				<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px] ">
-					<img
-						src="/cover.png"
-						alt="cover"
-						className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
-					/>
-				</div>
+				<div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px] " />
+				<img
+					src="/cover.png"
+					alt="cover"
+					className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+				/>
+				<a href="#explore">
+					<div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+						<img
+							src="/stamp.png"
+							alt="stamp"
+							className="w-[100px] sm:w-[150px] h-[100px] sm:h-[150px] object-contain"
+						/>
+					</div>
+				</a>
 			</motion.div>
 		</motion.div>
 	</section>
