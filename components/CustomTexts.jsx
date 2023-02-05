58a8c56d429d77548310c3c73be46a53c1346e15
ashishxcode <ashish.patel0903@gmail.com>
2023-02-05 17:55:18 +0530

    CI : Created Typing Text Component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,20 @@
 'use client';
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+export const TypingText = ({ title, textStyle }) => (
+	<motion.p
+		variants={textContainer}
+		initial="hidden"
+		whileInView="show"
+		className={`${textStyle} font-normal text-[14px] text-secondary-white`}
+	>
+		{Array.from(title).map((letter, index) => (
+			<motion.span variants={textVariant2} key={index}>
+				{letter === ' ' ? '\u00A0' : letter}
+			</motion.span>
+		))}
+	</motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = ({ title, textStyle }) => <h2>Title Text</h2>;
--- sections/About.jsx
+++ sections/About.jsx
@@ -1,9 +1,22 @@
 'use client';
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
-  <section>
-    About section
-  </section>
+	<section className={`${styles.paddings} relative z-10`}>
+		<div className="gradient-02 z-0" />
+		<motion.div
+			variants={staggerContainer}
+			initial="hidden"
+			whileInView="show"
+			viewport={{ once: false, amount: 0.25 }}
+			className={`${styles.innerWidth} ${styles.flexCenter} mx-auto flex-col`}
+		>
+			<TypingText title=" | About Metaverses" textStyle="text-center" />
+		</motion.div>
+	</section>
 );
 
 export default About;
