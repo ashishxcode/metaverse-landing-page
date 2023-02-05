72b5563320eb54a86ee5b40bcae25ca456002af2
ashishxcode <ashish.patel0903@gmail.com>
2023-02-06 00:30:50 +0530

    CI : Adding META data

--- app/head.js
+++ app/head.js
@@ -1,9 +1,21 @@
 const Head = () => (
-  <>
-    <title>Metaversus</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
-  </>
+	<>
+		<title>METAVERSE Landing page design by Ashish Patel</title>
+		<meta
+			name="description"
+			content="METAVERSE Landing page design by Ashish Patel"
+		/>
+		<meta
+			name="keywords"
+			content="METAVERSE Landing page design by Ashish Patel"
+		/>
+		<meta name="author" content="Ashish Patel" />
+		<meta name="robots" content="index, follow" />
+		<meta name="googlebot" content="index, follow" />
+		<meta name="google" content="nositelinkssearchbox" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" href="/favicon.ico" />
+	</>
 );
 
 export default Head;
