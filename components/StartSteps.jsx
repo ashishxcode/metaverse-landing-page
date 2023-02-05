04c8c5de6231414cff5292f4c0778a6b6fbddf03
ashishxcode <ashish.patel0903@gmail.com>
2023-02-05 23:46:52 +0530

    CI : Completed Get Started Component

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,7 +1,15 @@
-const StartSteps = () => (
-  <div>
-    start steps
-  </div>
+import styles from '../styles';
+const StartSteps = ({ text, number }) => (
+	<div className={`${styles.flexCenter} flex-row`}>
+		<div
+			className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[20px] bg-[#323f5d]`}
+		>
+			<p className="text-white text-[18px] font-bold">0{number}</p>
+		</div>
+		<p className="flex-1 ml-[20px] text-[18px] leading-[27.6px] font-normal text-[#b0b0b0]">
+			{text}
+		</p>
+	</div>
 );
 
 export default StartSteps;
--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,9 +1,43 @@
 'use client';
+import { motion } from 'framer-motion';
+import { StartSteps, TitleText, TypingText } from '../components';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { startingFeatures } from '../constants/';
+import styles from '../styles';
 
 const GetStarted = () => (
-  <section>
-    Get Started Section
-  </section>
+	<section className={`${styles.paddings}relative z-10`}>
+		<motion.div
+			variants={staggerContainer}
+			initial="hidden"
+			whileInView="show"
+			viewport={{ once: false, amount: 0.25 }}
+			className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8 `}
+		>
+			<motion.div
+				variants={planetVariants('left')}
+				className={`flex-1 ${styles.flexCenter}`}
+			>
+				<img
+					src="/get-started.png"
+					alt="get started"
+					className="w-[90%] h-[90%] object-contain"
+				/>
+			</motion.div>
+			<motion.div
+				variants={fadeIn('left', 'tween', 0.2, 1)}
+				className="flex-[0.75] flex flex-col justify-center"
+			>
+				<TypingText title="How Metaverse Works" textStyle="text-[20px]" />
+				<TitleText title={<>Get Started in 60 sec.</>} />
+				<div className="flex flex-col gap-[24px] max-w-[370px] mt-[30px]">
+					{startingFeatures.map((feature, index) => (
+						<StartSteps key={index} text={feature} number={index + 1} />
+					))}
+				</div>
+			</motion.div>
+		</motion.div>
+	</section>
 );
 
 export default GetStarted;
