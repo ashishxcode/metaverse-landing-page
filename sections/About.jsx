f945ceb2c3665f239184b5c7c0f2c127156a0c09
ashishxcode <ashish.patel0903@gmail.com>
2023-02-05 20:46:11 +0530

    CI : Adding About Section

--- sections/About.jsx
+++ sections/About.jsx
@@ -14,7 +14,34 @@ const About = () => (
 			viewport={{ once: false, amount: 0.25 }}
 			className={`${styles.innerWidth} ${styles.flexCenter} mx-auto flex-col`}
 		>
-			<TypingText title=" | About Metaverses" textStyle="text-center" />
+			<TypingText
+				title="About Metaverses"
+				textStyle="text-center text-[20px]"
+			/>
+			<motion.p
+				variants={fadeIn('up', 'tween', 0.2, 1)}
+				className="text-center mt-6 font-normal sm:text-[24px] text-[20px] text-secondary-white"
+			>
+				<span className="font-extrabold text-white">Metaverse </span>
+				is a new things in the future, where you can enjoy the
+				<span className="font-extrabold text-white"> Virtual Reality </span>
+				world by feeling like you are in the
+				<span className="font-extrabold text-white"> real world. </span> you can
+				do anything in the metaverse, such as playing games, watching movies,
+				and even working. you can also meet new people and make new friends in
+				the metaverse. this is the future of the world. this is the
+				<span className="font-extrabold text-white">
+					{' '}
+					metaverse of the madness.{' '}
+				</span>
+				Let's explore the metaverse together. by scrolling down, you can see the
+				metaverse.
+			</motion.p>
+			<motion.img
+				src="/arrow-down.svg"
+				variants={fadeIn('up', 'tween', 0.3, 1)}
+				className="w-6 h-6 mt-8 animate-bounce"
+			/>
 		</motion.div>
 	</section>
 );
